aa8cc3d3d45d46e70f0aadf1eecc0d9571f956d0
maciejKolanski <maciej.kolanski@gmail.com>
2018-04-19 13:03:36 -0700

    working hard

--- acquirer/src/main/java/acquirer/Acquirer.java
+++ acquirer/src/main/java/acquirer/Acquirer.java
@@ -1,14 +1,13 @@
 package acquirer;
 
 import java.util.*;
-
 import com.mongodb.MongoClient;
 import com.mongodb.client.*;
 
 public class Acquirer {
-	public static final String population_name = "population";
-	public static final String gdp_name = "gdp";
-	public static final String education_name = "education";
+	public static final String education_collection 	= "education";
+	public static final String gdp_collection 			= "gdp";
+	public static final String population_collection 	= "population";
 	
 	private static Boolean hasCollection(MongoDatabase db, String collection) {
 		final MongoCursor<String> it = db.listCollectionNames().iterator();
@@ -21,7 +20,7 @@ public class Acquirer {
 	}
 	
 	private static void prepareCollections(MongoDatabase db) {
-		final List<String> collectionsToCreate = Arrays.asList(population_name, gdp_name, education_name);
+		final List<String> collectionsToCreate = Arrays.asList(education_collection, gdp_collection, population_collection);
 		
 		for (final String collectionToCreate : collectionsToCreate) {
 	        if (!hasCollection(db, collectionToCreate))
@@ -29,12 +28,15 @@ public class Acquirer {
 	    }
 	}
 	
-	
 	public static void main(String[] args) {
 		MongoClient mongoClient = new MongoClient();
 		MongoDatabase db = mongoClient.getDatabase("bigdata");
 			
 		prepareCollections(db);
+				
+		(new Thread(new CSVParser("/home/cloudera/workspace/bigdata/tests/functional/same_year/education.csv"))).start();
+		
+		mongoClient.close();
 	}
 
 }
